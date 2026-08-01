f0bca4138fcc5d1bb83b2149ba4c6a92cf70a811
riyageorge4128-ai <riyageorge4128@gmail.com>
2026-08-01 10:30:00 +0530

    Fix PrimeCheck and organize LabCycle1 programs into dedicated module

---
+++ LabCycle1/PrimeCheck.java
@@ -0,0 +1,23 @@
+﻿import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Number= ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        if (count == 2) {
+            System.out.print(num + " is Prime");
+        } else {
+            System.out.print(num + " is Not Prime");
+        }
+    }
+}
